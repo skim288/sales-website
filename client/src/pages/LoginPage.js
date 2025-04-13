3a685d8d59f64caed2eae537931e0f6c57891e2d
skim288 <stephenkim288@gmail.com>
2025-04-12 21:27:48 -0700

    Initial commit

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,34 @@
+import SocialLogin from "../components/SocialLogin";
+import InputField from "../components/InputField";
+
+export default function LoginPage() {
+  return (
+    <div className="login-container">
+      <h2 className="form-title">Log in with</h2>
+      <SocialLogin />
+
+      <p className="separator">
+        <span>or</span>
+      </p>
+
+      <form action="#" className="login-form">
+        <InputField type="email" placeholder="Email address" />
+        <InputField type="password" placeholder="Password" />
+
+        <a href="#" className="forgot-password-link">
+          Forgot password?
+        </a>
+        <button type="submit" className="login-button">
+          Log In
+        </button>
+      </form>
+
+      <p className="signup-prompt">
+        Don&apos;t have an account?{" "}
+        <a href="#" className="signup-link">
+          Sign up
+        </a>
+      </p>
+    </div>
+  );
+}
