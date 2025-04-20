54dd5fe14d6ff572a22f89f0e0b92510a4ee4d0e
skim288 <165584958+skim288@users.noreply.github.com>
2025-04-20 16:07:07 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,6 @@ import { createTheme } from "@mui/material/styles";
 import { AuthProvider, useAuth } from './components/AuthContext';
 import NavBar from './components/NavBar';
 import HomePage from './pages/HomePage';
-import AlbumsPage from './pages/AlbumsPage';
-import SongsPage from './pages/SongsPage';
-import AlbumInfoPage from './pages/AlbumInfoPage';
 import SalesPage from './pages/SalesPage';
 import CustomerPage from './pages/CustomerPage';
 import LoginPage from './pages/LoginPage';
@@ -58,14 +55,7 @@ export default function App() {
                 </ProtectedRoute>
               } 
             />
-            <Route 
-              path="/albums/:album_id" 
-              element={
-                <ProtectedRoute>
-                  <AlbumInfoPage />
-                </ProtectedRoute>
-              } 
-            />
+            
             <Route 
               path="/customer_dashboard" 
               element={
